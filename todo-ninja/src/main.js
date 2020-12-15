152694d831ba2ebae662489e39019695ab7b6be1
Moha Specter <drame.moha44@gmail.com>
2020-12-15 18:47:47 +0100

    json correction

--- todo-ninja/src/main.js
+++ todo-ninja/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import './plugins/vuetify'
+import './plugins/vuetify.js'
 import App from './App.vue'
-import router from './router'
+import router from './router.js'
 
 Vue.config.productionTip = false
 
